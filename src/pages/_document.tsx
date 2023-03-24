80675bfbe84f3b1642f2b4734ad818e7ed1a3d29
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-24 15:40:31 -0500

    add global theme

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+function _document() {
+  return (
+    <Html data-theme="night">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+export default _document;
