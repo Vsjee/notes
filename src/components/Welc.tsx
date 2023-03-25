57054bae628ccafd6625ee94e2dfacc3d808425b
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-25 17:47:13 -0500

    add welc comp

---
+++ src/components/Welc.tsx
@@ -0,0 +1,12 @@
+function Welc() {
+  return (
+    <section className="m-5 mt-[10rem] grid place-content-center">
+      <h1 className="text-center text-4xl">Welcome to notes</h1>
+      <p className="pt-5 text-center">
+        Here you could take some notes and saved to see later!.
+      </p>
+    </section>
+  );
+}
+
+export default Welc;
